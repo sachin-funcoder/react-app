60f152664f320c3d96ec4f7e621a0edcf84eb341
sachin-funcoder <sachi.m1289@gmail.com>
2026-04-17 14:22:49 +0530

    ToDo list

---
+++ todo/src/components/Input.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+function Input({addTodo}) {
+
+    const [todo,setTodo] = useState("")
+
+    const handleChange = () =>{
+        if(todo.trim() !== ""){
+           addTodo(todo)
+           setTodo("")
+        }
+    }
+
+  return (
+    <div>
+    <input type="text" placeholder='Enter your todo' value={todo} onChange={e=>setTodo(e.target.value)}/>
+    <button onClick={handleChange}>Add</button>
+    </div>
+  )
+}
+
+export default Input
